b377279494eb7db958eaeb8a7a58f720e8c102ec
Luis Cabarique <luis.cabarique@mercadoni.com>
2019-04-08 19:50:38 -0500

    feat: setup GameBoardViewController

---
+++ Connect4/GameBoardViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GameBoardViewController.swift
+//  Connect4
+//
+//  Created by Luis Cabarique on 4/8/19.
+//  Copyright © 2019 Luis Cabarique. All rights reserved.
+//
+
+import UIKit
+
+class GameBoardViewController: UIViewController {
+    
+}
